9546eea882ee085e0e516086e32a6688ca22ebb0
AAsriyan <aasriyan@protonmail.com>
2019-02-01 12:40:17 -0500

    Fixed the bug in Tabs.js, MVP completed

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,19 +2,20 @@ class TabLink {
   constructor(tabElement){
     // assign this.tabElement to the tabElement DOM reference
     this.tabElement = tabElement;
-    console.log(tabElement);
+    //console.log(tabElement);
     // Get the `data-tab` value from this.tabElement and store it here
     this.tabData = this.tabElement.dataset.tab; 
     //console.log(this.tabData);
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
-    
+    // this.cards = document.querySelectorAll(`.card[data-tab='${this.tabElement.dataset.tab}']`)
+    // console.log(this.cards);
     // Check to see if this.tabData is equal to 'all'
     if(this.tabData === 'all'){
       // If `all` is true, select all cards regardless of their data attribute values
-      this.cards = document.querySelectorAll('.tab');
+      this.cards = document.querySelectorAll('.card');
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      this.cards = this.tabElement.querySelectorAll(`.card[data-tab='${this.data}']`);
+      this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`);
     }
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
@@ -28,13 +29,13 @@ class TabLink {
 
     // Select all elements with the .tab class on them
     const tabs = document.querySelectorAll('.tab');
-    
+    //console.log(tabs);
     // Iterate through the NodeList removing the .active-tab class from each element
     tabs.forEach(tab => tab.classList.remove('active-tab'));
 
     // Select all of the elements with the .card class on them
     const cards = document.querySelectorAll('.card');
-
+    //console.log(cards);
     // Iterate through the NodeList setting the display style each one to 'none'
     cards.forEach(card => card.style.display = 'none');
     
@@ -50,6 +51,7 @@ class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
     this.cardElement = cardElement;
+    //console.log(this.cardElement);
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
